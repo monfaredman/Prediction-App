161613b26aff46cd5ba9daf7ff574f14abca9450
moslem <moslemhosseinpour1998@gmail.com>
2022-11-14 18:27:50 +0330

    add new i

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,29 +1,65 @@
-import { Person } from "@mui/icons-material";
-import React from "react";
+import { useState, useEffect } from "react";
+import CountriesData from "assets/data/countries.json";
 
 export default function Countries() {
-  return <div>Countries</div>;
-}
-
-function getCountryDetail() {
-  let url = "https://en.wikipedia.org/w/api.php";
-  const params: {
-    [x: string]: string;
-    action: string;
-    prop: string;
-    titles: string;
-    format: string;
-  } = {
-    action: "query",
-    prop: "images",
-    titles: "Albert Einstein",
-    format: "json",
+  type Employee = {
+    [x: string]: any;
+    source: string;
+    height: number;
+    width: number;
   };
-
-  url = url + "?origin=*";
-  Object.keys(params).forEach(function (key) {
-    url += "&" + key + "=" + params.key;
+  const [countryFlag, setCountryFlag] = useState<Employee[]>([]);
+  useEffect(() => {
+    getCountryDetail();
+  }, []);
+  const getCountryDetail = async () => {
+    let foundObj: any = [];
+    CountriesData.countries.forEach(async (element) => {
+      const response = await fetch(createUrl(element.Name));
+      const movies = await response.json();
+      function findNestedObj(entireObj: Object, keyToFind: any) {
+        JSON.stringify(entireObj, (_, nestedValue) => {
+          if (nestedValue && nestedValue[keyToFind]) {
+            foundObj.push(nestedValue);
+          }
+        });
+      }
+      findNestedObj(movies.query.pages[0].thumbnail, "source");
+    });
+    setCountryFlag((old) => [...old, foundObj]);
+  };
+  const createUrl = (element: string) => {
+    let url = "https://en.wikipedia.org/w/api.php?origin=*";
+    const params: {
+      [x: string]: string;
+      action: string;
+      prop: string;
+      titles: string;
+      format: string;
+      formatversion: string;
+    } = {
+      action: "query",
+      prop: "pageimages",
+      titles: element,
+      format: "json",
+      piprop: "thumbnail&pithumbsize=500",
+      formatversion: "2",
+    };
+    Object.keys(params).forEach(function (key) {
+      url += "&" + key + "=" + params[key];
+    });
+    return url;
+  };
+  const mapImage = countryFlag.map((x) => {
+    console.log(x);
+    return x.map((item: { source: string }) => {
+      if (item) {
+        return <img src={item.source} alt={item.source} key={item.source} />;
+      } else {
+        return <p className="text-white">sdsd</p>;
+      }
+    });
   });
-  console.log(url);
+
+  return <div>{mapImage} && null</div>;
 }
-getCountryDetail();
